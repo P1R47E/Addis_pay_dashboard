70798fdc04336bc6a34ef6fa0fa3c9a99058d1b5
P1R47E <kingzaljaguar@gmail.com>
2024-12-04 11:00:50 +0300

    sidenav added

---
+++ Addis_pay/src/components/Header.jsx
@@ -0,0 +1,34 @@
+function Header(){
+    return(
+     <div className="bg-white p-4 border-b">
+        <div className="flex justify-between items-center">
+            <div className="flex items-center space-x-4">
+                <h2 className="text-xl">💳 Instant Payments</h2>
+            </div>
+            <div className="flex items-center space-x-4">
+                <button className="bg-emerald-500 text-white px-4 py-2 rounded">
+                    Test mode
+                </button>
+                <div className="relative">
+                    <span classname="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center ">
+                        2
+                    </span>
+                    <span>🔔</span>
+                </div>
+                <div className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center " >
+                 HB
+                </div> 
+            </div>
+        </div>
+        <div className="mt-4">
+            <div classname="flex space-x-4">
+                <button className="border-b-2 border-emerald-500 pb-2">To pay</button>
+                <button className="text-gray-500 ml-4">My requests</button> 
+
+            </div>
+        </div>
+     </div>   
+    );
+}
+
+export default Header
---
+++ Addis_pay/src/components/Sidenav.jsx
@@ -0,0 +1,28 @@
+function Sidenav(){
+    const menuItems  = [
+    { icon: "⬜", label: "Dashboard" },
+    { icon: "🏦", label: "Banks" },
+    { icon: "👥", label: "Customers" },
+    { icon: "📝", label: "Requests" },
+    { icon: "💳", label: "Instant Payment", active: true },
+    { icon: "📒", label: "Ledgers" },
+    { icon: "💰", label: "Settlements" },
+    { icon: "📥", label: "Offline Deposit" },
+    ];
+    return (
+        <div className="w-64 bg-emerald-500 text-white min-h-screen">
+            <div className="p-4">
+                <h1 className="text-2x1 font-bold mb-8">Addispay</h1>
+                <nav>
+                    {menuItems.map((item,index) =>(
+                        <div key = {index} className={`flex items-center p-3 mb-2 rounded ${item.active ? 'bg-white text-emerald-500' : 'hover: bg-emerald-600'}`}>
+                            <span className="mr-3">{item.icon}</span>
+                            <span>{item.label}</span>
+                        </div>
+                    ))}
+                </nav>
+            </div>
+        </div>
+    )
+}
+export default Sidenav
